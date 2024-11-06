a7aa9ce0a98e6d705aec0c7487cd5c36c29fe6fe
Chen Xi <xichen0425@gmail.com>
2024-11-05 20:40:12 -0800

    Use direct HTTP for private key usage

--- pkg/provider/okta.go
+++ pkg/provider/okta.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -15,27 +17,30 @@ import (
 	"github.com/go-jose/go-jose/v3/jwt"
 	"github.com/google/uuid"
 	"github.com/puppetlabs/leg/errmap/pkg/errmark"
-	"github.com/puppetlabs/vault-plugin-secrets-oauthapp/v3/pkg/oauth2ext/clientctx"
-	"github.com/puppetlabs/vault-plugin-secrets-oauthapp/v3/pkg/oauth2ext/devicecode"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/puppetlabs/vault-plugin-secrets-oauthapp/v3/pkg/oauth2ext/devicecode"
 )
 
 const (
 	OptionDomain     = "domain"
 	OptionPrivateKey = "private_key"
-	OptionScheme     = "scheme" // Add this for testing
+	OptionScheme     = "scheme"
+	OktaProviderV1   = 1
 
-	JWTExpirationTime       = time.Hour
-	JWTAssertionType        = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
-	JWTClientAssertionParam = "client_assertion"
-	JWTAssertionTypeParam   = "client_assertion_type"
-
-	OktaProviderV1 = 1
+	JWTExpirationTime = time.Hour
 )
 
 func init() {
 	GlobalRegistry.MustRegister("okta", OktaFactory)
+}
+
+type oktaToken struct {
+	TokenType   string `json:"token_type,omitempty"`
+	ExpiresIn   int    `json:"expires_in,omitempty"`
+	AccessToken string `json:"access_token,omitempty"`
+	Scope       string `json:"scope,omitempty"`
 }
 
 type oktaOperations struct {
@@ -45,11 +50,20 @@ type oktaOperations struct {
 	clientSecret    string
 	privateKey      *rsa.PrivateKey
 	usePrivateKey   bool
+	httpClient      *http.Client
 }
 
-func (o *oktaOperations) generateJWT() (string, error) {
+func (o *oktaOperations) createClientAssertion() (string, error) {
 	if !o.usePrivateKey || o.privateKey == nil {
 		return "", fmt.Errorf("private key not configured")
+	}
+
+	signer, err := jose.NewSigner(
+		jose.SigningKey{Algorithm: jose.RS256, Key: o.privateKey},
+		(&jose.SignerOptions{}).WithType("JWT"),
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to create signer: %w", err)
 	}
 
 	now := time.Now()
@@ -63,20 +77,105 @@ func (o *oktaOperations) generateJWT() (string, error) {
 		ID:        uuid.New().String(),
 	}
 
-	signer, err := jose.NewSigner(
-		jose.SigningKey{Algorithm: jose.RS256, Key: o.privateKey},
-		(&jose.SignerOptions{}).WithType("JWT"),
-	)
-	if err != nil {
-		return "", fmt.Errorf("failed to create signer: %w", err)
-	}
-
 	token, err := jwt.Signed(signer).Claims(claims).CompactSerialize()
 	if err != nil {
 		return "", fmt.Errorf("failed to create JWT: %w", err)
 	}
 
 	return token, nil
+}
+
+func (o *oktaOperations) ClientCredentials(ctx context.Context, opts ...ClientCredentialsOption) (*Token, error) {
+	options := &ClientCredentialsOptions{}
+	options.ApplyOptions(opts)
+
+	endpoint := o.endpointFactory(options.ProviderOptions)
+
+	if o.usePrivateKey {
+		// Private Key JWT - Direct HTTP request
+		clientAssertion, err := o.createClientAssertion()
+		if err != nil {
+			return nil, fmt.Errorf("failed to create client assertion: %w", err)
+		}
+
+		data := url.Values{}
+		data.Set("grant_type", "client_credentials")
+		data.Set("scope", strings.Join(options.Scopes, " "))
+		data.Set("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer")
+		data.Set("client_assertion", clientAssertion)
+
+		req, err := http.NewRequestWithContext(ctx, "POST", endpoint.TokenURL, strings.NewReader(data.Encode()))
+		if err != nil {
+			return nil, fmt.Errorf("failed to create request: %w", err)
+		}
+
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		resp, err := o.httpClient.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("failed to send request: %w", err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read response: %w", err)
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			var errResp struct {
+				Error            string `json:"error"`
+				ErrorDescription string `json:"error_description"`
+			}
+			if err := json.Unmarshal(body, &errResp); err == nil {
+				return nil, fmt.Errorf("authentication failed: %s, %s", errResp.Error, errResp.ErrorDescription)
+			}
+			return nil, fmt.Errorf("request failed with status %d: %s", resp.StatusCode, string(body))
+		}
+
+		var oktaToken oktaToken
+		if err := json.Unmarshal(body, &oktaToken); err != nil {
+			return nil, fmt.Errorf("failed to parse response: %w", err)
+		}
+
+		token := &oauth2.Token{
+			AccessToken: oktaToken.AccessToken,
+			TokenType:   oktaToken.TokenType,
+			Expiry:      time.Now().Add(time.Duration(oktaToken.ExpiresIn) * time.Second),
+		}
+
+		return &Token{
+			Token:           token,
+			ProviderVersion: o.vsn,
+			ProviderOptions: options.ProviderOptions,
+		}, nil
+	}
+
+	// Client Secret - Use clientcredentials package
+	if o.clientSecret == "" {
+		return nil, errmark.MarkUser(ErrMissingClientSecret)
+	}
+
+	cc := &clientcredentials.Config{
+		ClientID:       o.clientID,
+		ClientSecret:   o.clientSecret,
+		TokenURL:       endpoint.TokenURL,
+		Scopes:         options.Scopes,
+		EndpointParams: options.EndpointParams,
+		AuthStyle:      endpoint.AuthStyle,
+	}
+
+	tok, err := cc.Token(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Token{
+		Token:           tok,
+		ProviderVersion: o.vsn,
+		ProviderOptions: options.ProviderOptions,
+	}, nil
 }
 
 func (o *oktaOperations) AuthCodeURL(state string, opts ...AuthCodeURLOption) (string, bool) {
@@ -107,57 +206,11 @@ func (o *oktaOperations) DeviceCodeExchange(ctx context.Context, deviceCode stri
 }
 
 func (o *oktaOperations) AuthCodeExchange(ctx context.Context, code string, opts ...AuthCodeExchangeOption) (*Token, error) {
-	if o.clientSecret == "" && !o.usePrivateKey {
-		return nil, errmark.MarkUser(ErrMissingClientSecret)
-	}
+	return nil, fmt.Errorf("auth code exchange flow not supported")
+}
 
-	options := &AuthCodeExchangeOptions{}
-	options.ApplyOptions(opts)
-
-	endpoint := o.endpointFactory(options.ProviderOptions)
-
-	var tok *oauth2.Token
-	var err error
-
-	if o.usePrivateKey {
-		assertion, err := o.generateJWT()
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate JWT: %w", err)
-		}
-
-		cfg := &oauth2.Config{
-			Endpoint:    endpoint.Endpoint,
-			ClientID:    o.clientID,
-			RedirectURL: options.RedirectURL,
-		}
-
-		opts := []oauth2.AuthCodeOption{
-			oauth2.SetAuthURLParam(JWTAssertionTypeParam, JWTAssertionType),
-			oauth2.SetAuthURLParam(JWTClientAssertionParam, assertion),
-		}
-		opts = append(opts, options.AuthCodeOptions...)
-
-		tok, err = cfg.Exchange(ctx, code, opts...)
-	} else {
-		cfg := &oauth2.Config{
-			Endpoint:     endpoint.Endpoint,
-			ClientID:     o.clientID,
-			ClientSecret: o.clientSecret,
-			RedirectURL:  options.RedirectURL,
-		}
-
-		tok, err = cfg.Exchange(ctx, code, options.AuthCodeOptions...)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &Token{
-		Token:           tok,
-		ProviderVersion: o.vsn,
-		ProviderOptions: options.ProviderOptions,
-	}, nil
+func (o *oktaOperations) TokenExchange(ctx context.Context, t *Token, opts ...TokenExchangeOption) (*Token, error) {
+	return nil, fmt.Errorf("token exchange flow not supported")
 }
 
 func (o *oktaOperations) RefreshToken(ctx context.Context, t *Token, opts ...RefreshTokenOption) (*Token, error) {
@@ -167,190 +220,15 @@ func (o *oktaOperations) RefreshToken(ctx context.Context, t *Token, opts ...Ref
 
 	endpoint := o.endpointFactory(options.ProviderOptions)
 
-	var tok *oauth2.Token
-	var err error
-
-	if o.usePrivateKey {
-		assertion, err := o.generateJWT()
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate JWT: %w", err)
-		}
-
-		cfg := &oauth2.Config{
-			Endpoint: endpoint.Endpoint,
-			ClientID: o.clientID,
-		}
-
-		ctx = context.WithValue(ctx, oauth2.HTTPClient, oauth2.NewClient(ctx, nil))
-		ctx = context.WithValue(ctx, oauth2.HTTPClient, &http.Client{
-			Transport: &oauth2.Transport{
-				Source: oauth2.ReuseTokenSource(nil, oauth2.StaticTokenSource(&oauth2.Token{
-					AccessToken: assertion,
-					TokenType:   "urn:ietf:params:oauth:token-type:jwt",
-				})),
-			},
-		})
-
-		tok, err = cfg.TokenSource(ctx, &oauth2.Token{
-			RefreshToken: t.RefreshToken,
-		}).Token()
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		if o.clientSecret == "" {
-			return nil, errmark.MarkUser(ErrMissingClientSecret)
-		}
-
-		cfg := &oauth2.Config{
-			Endpoint:     endpoint.Endpoint,
-			ClientID:     o.clientID,
-			ClientSecret: o.clientSecret,
-		}
-
-		tok, err = cfg.TokenSource(ctx, &oauth2.Token{
-			RefreshToken: t.RefreshToken,
-		}).Token()
+	cfg := &oauth2.Config{
+		Endpoint:     endpoint.Endpoint,
+		ClientID:     o.clientID,
+		ClientSecret: o.clientSecret,
 	}
 
-	if err != nil {
-		return nil, err
-	}
-
-	return &Token{
-		Token:           tok,
-		ProviderVersion: o.vsn,
-		ProviderOptions: options.ProviderOptions,
-	}, nil
-}
-
-func (o *oktaOperations) ClientCredentials(ctx context.Context, opts ...ClientCredentialsOption) (*Token, error) {
-	options := &ClientCredentialsOptions{}
-	options.ApplyOptions(opts)
-
-	endpoint := o.endpointFactory(options.ProviderOptions)
-
-	if o.usePrivateKey {
-		assertion, err := o.generateJWT()
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate JWT: %w", err)
-		}
-
-		if options.EndpointParams == nil {
-			options.EndpointParams = url.Values{}
-		}
-		options.EndpointParams.Set(JWTAssertionTypeParam, JWTAssertionType)
-		options.EndpointParams.Set(JWTClientAssertionParam, assertion)
-
-		cc := &clientcredentials.Config{
-			ClientID:       o.clientID,
-			TokenURL:       endpoint.TokenURL,
-			Scopes:         options.Scopes,
-			EndpointParams: options.EndpointParams,
-			AuthStyle:      oauth2.AuthStyleInParams,
-		}
-
-		tok, err := cc.Token(ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		return &Token{
-			Token:           tok,
-			ProviderVersion: o.vsn,
-			ProviderOptions: options.ProviderOptions,
-		}, nil
-	}
-
-	if o.clientSecret == "" {
-		return nil, errmark.MarkUser(ErrMissingClientSecret)
-	}
-
-	cc := &clientcredentials.Config{
-		ClientID:       o.clientID,
-		ClientSecret:   o.clientSecret,
-		TokenURL:       endpoint.TokenURL,
-		Scopes:         options.Scopes,
-		EndpointParams: options.EndpointParams,
-		AuthStyle:      endpoint.AuthStyle,
-	}
-
-	tok, err := cc.Token(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Token{
-		Token:           tok,
-		ProviderVersion: o.vsn,
-		ProviderOptions: options.ProviderOptions,
-	}, nil
-}
-
-func (o *oktaOperations) TokenExchange(ctx context.Context, t *Token, opts ...TokenExchangeOption) (*Token, error) {
-	options := &TokenExchangeOptions{}
-	options.ApplyOptions(opts)
-
-	endpoint := o.endpointFactory(options.ProviderOptions)
-
-	var cfg *oauth2.Config
-	var exchangeOpts []oauth2.AuthCodeOption
-
-	if o.usePrivateKey {
-		assertion, err := o.generateJWT()
-		if err != nil {
-			return nil, fmt.Errorf("failed to generate JWT: %w", err)
-		}
-
-		cfg = &oauth2.Config{
-			Endpoint: endpoint.Endpoint,
-			ClientID: o.clientID,
-		}
-
-		exchangeOpts = append(exchangeOpts,
-			oauth2.SetAuthURLParam(JWTAssertionTypeParam, JWTAssertionType),
-			oauth2.SetAuthURLParam(JWTClientAssertionParam, assertion),
-		)
-	} else {
-		if o.clientSecret == "" {
-			return nil, errmark.MarkUser(ErrMissingClientSecret)
-		}
-
-		cfg = &oauth2.Config{
-			Endpoint:     endpoint.Endpoint,
-			ClientID:     o.clientID,
-			ClientSecret: o.clientSecret,
-		}
-	}
-
-	ctx = clientctx.WithUpdatedRequestBody(ctx, func(body []byte) ([]byte, error) {
-		vs, err := url.ParseQuery(string(body))
-		if err != nil {
-			return nil, err
-		}
-
-		if len(options.Audiences) > 0 {
-			vs["audience"] = options.Audiences
-		}
-		if len(options.Resources) > 0 {
-			vs["resource"] = options.Resources
-		}
-
-		return []byte(vs.Encode()), nil
-	})
-
-	exchangeOpts = append(exchangeOpts,
-		oauth2.SetAuthURLParam("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange"),
-		oauth2.SetAuthURLParam("subject_token_type", "urn:ietf:params:oauth:token-type:access_token"),
-		oauth2.SetAuthURLParam("subject_token", t.AccessToken),
-		oauth2.SetAuthURLParam("requested_token_type", "urn:ietf:params:oauth:token-type:access_token"),
-	)
-
-	if len(options.Scopes) > 0 {
-		exchangeOpts = append(exchangeOpts, oauth2.SetAuthURLParam("scope", strings.Join(options.Scopes, " ")))
-	}
-
-	tok, err := cfg.Exchange(ctx, "", append(exchangeOpts, options.AuthCodeOptions...)...)
+	tok, err := cfg.TokenSource(ctx, &oauth2.Token{
+		RefreshToken: t.RefreshToken,
+	}).Token()
 	if err != nil {
 		return nil, err
 	}
@@ -385,6 +263,7 @@ func (o *okta) Private(clientID, clientSecret string) PrivateOperations {
 		clientSecret:    clientSecret,
 		privateKey:      o.privateKey,
 		usePrivateKey:   o.usePrivateKey,
+		httpClient:      &http.Client{},
 	}
 }
 
@@ -402,12 +281,6 @@ func OktaFactory(ctx context.Context, vsn int, opts map[string]string) (Provider
 		return nil, &OptionError{Option: OptionDomain, Cause: fmt.Errorf("domain is required")}
 	}
 
-	// Default to https unless specified otherwise (for testing)
-	scheme := "https"
-	if s := opts[OptionScheme]; s != "" {
-		scheme = s
-	}
-
 	var privateKey *rsa.PrivateKey
 	usePrivateKey := false
 
@@ -417,13 +290,24 @@ func OktaFactory(ctx context.Context, vsn int, opts map[string]string) (Provider
 			return nil, &OptionError{Option: OptionPrivateKey, Cause: fmt.Errorf("failed to parse PEM block")}
 		}
 
-		key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+		// Only support PKCS8
+		key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 		if err != nil {
-			return nil, &OptionError{Option: OptionPrivateKey, Cause: fmt.Errorf("failed to parse private key: %w", err)}
+			return nil, &OptionError{Option: OptionPrivateKey, Cause: fmt.Errorf("failed to parse PKCS8 private key: %w", err)}
 		}
 
-		privateKey = key
+		var ok bool
+		privateKey, ok = key.(*rsa.PrivateKey)
+		if !ok {
+			return nil, &OptionError{Option: OptionPrivateKey, Cause: fmt.Errorf("key is not an RSA private key")}
+		}
+
 		usePrivateKey = true
+	}
+
+	scheme := opts[OptionScheme]
+	if scheme == "" {
+		scheme = "https"
 	}
 
 	p := &okta{
--- pkg/provider/okta_test.go
+++ pkg/provider/okta_test.go
@@ -6,322 +6,146 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"strings"
 	"testing"
 
-	"github.com/go-jose/go-jose/v3/jwt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/oauth2"
 )
 
-func generateTestRSAKey(t *testing.T) (string, *rsa.PrivateKey) {
+func generateTestPKCS8Key(t *testing.T) (string, *rsa.PrivateKey) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	require.NoError(t, err)
 
-	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	require.NoError(t, err)
+
 	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PRIVATE KEY",
+		Type:  "PRIVATE KEY",
 		Bytes: privateKeyBytes,
 	})
+
 	return string(privateKeyPEM), privateKey
 }
-
-type testServer struct {
-	t              *testing.T
-	server         *httptest.Server
-	expectedTokens map[string]bool
-	claims         *jwt.Claims
-}
-
-func newTestServer(t *testing.T) *testServer {
-	ts := &testServer{
-		t:              t,
-		expectedTokens: make(map[string]bool),
-	}
-
-	ts.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.URL.Path {
-		case "/oauth2/v1/token":
-			ts.handleToken(w, r)
-		case "/oauth2/v1/authorize":
-			ts.handleAuthorize(w, r)
-		default:
-			http.NotFound(w, r)
-		}
-	}))
-
-	return ts
-}
-
-func (ts *testServer) handleToken(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	require.NoError(ts.t, err)
-
-	grantType := r.Form.Get("grant_type")
-	switch grantType {
-	case "client_credentials":
-		ts.handleClientCredentials(w, r)
-	case "authorization_code":
-		ts.handleAuthCode(w, r)
-	case "refresh_token":
-		ts.handleRefreshToken(w, r)
-	case "urn:ietf:params:oauth:grant-type:token-exchange":
-		ts.handleTokenExchange(w, r)
-	default:
-		http.Error(w, "unsupported_grant_type", http.StatusBadRequest)
-	}
-}
-
-func (ts *testServer) handleClientCredentials(w http.ResponseWriter, r *http.Request) {
-	if assertionType := r.Form.Get("client_assertion_type"); assertionType != "" {
-		// JWT authentication
-		assertion := r.Form.Get("client_assertion")
-		if assertion == "" {
-			http.Error(w, "invalid_client", http.StatusUnauthorized)
-			return
-		}
-		// Validate JWT here if needed
-	} else {
-		// Client secret authentication
-		clientID := r.Form.Get("client_id")
-		clientSecret := r.Form.Get("client_secret")
-		if clientID != "test-client" || clientSecret != "test-secret" {
-			http.Error(w, "invalid_client", http.StatusUnauthorized)
-			return
-		}
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{
-		"access_token": "test-access-token",
-		"token_type": "Bearer",
-		"expires_in": 3600
-	}`)
-}
-
-func (ts *testServer) handleAuthCode(w http.ResponseWriter, r *http.Request) {
-	code := r.Form.Get("code")
-	if code != "valid-code" {
-		http.Error(w, "invalid_grant", http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{
-		"access_token": "test-access-token",
-		"refresh_token": "test-refresh-token",
-		"token_type": "Bearer",
-		"expires_in": 3600
-	}`)
-}
-
-func (ts *testServer) handleRefreshToken(w http.ResponseWriter, r *http.Request) {
-	refreshToken := r.Form.Get("refresh_token")
-	if refreshToken != "test-refresh-token" {
-		http.Error(w, "invalid_grant", http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{
-		"access_token": "new-access-token",
-		"refresh_token": "new-refresh-token",
-		"token_type": "Bearer",
-		"expires_in": 3600
-	}`)
-}
-
-func (ts *testServer) handleTokenExchange(w http.ResponseWriter, r *http.Request) {
-	subjectToken := r.Form.Get("subject_token")
-	if subjectToken != "test-access-token" {
-		http.Error(w, "invalid_grant", http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{
-		"access_token": "exchanged-access-token",
-		"token_type": "Bearer",
-		"expires_in": 3600
-	}`)
-}
-
-func (ts *testServer) handleAuthorize(w http.ResponseWriter, r *http.Request) {
-	clientID := r.URL.Query().Get("client_id")
-	redirectURI := r.URL.Query().Get("redirect_uri")
-	state := r.URL.Query().Get("state")
-
-	if clientID != "test-client" {
-		http.Error(w, "invalid_client", http.StatusBadRequest)
-		return
-	}
-
-	redirectURL, err := url.Parse(redirectURI)
-	require.NoError(ts.t, err)
-
-	q := redirectURL.Query()
-	q.Set("code", "valid-code")
-	q.Set("state", state)
-	redirectURL.RawQuery = q.Encode()
-
-	http.Redirect(w, r, redirectURL.String(), http.StatusFound)
-}
-
-func (ts *testServer) Close() {
-	ts.server.Close()
-}
-
-func TestOktaProvider(t *testing.T) {
-	ts := newTestServer(t)
-	defer ts.Close()
-
-	domain := strings.TrimPrefix(ts.server.URL, "http://")
-	keyPEM, _ := generateTestRSAKey(t)
+func TestOktaClientCredentials(t *testing.T) {
+	const (
+		testClientID = "test-client"
+	)
 
 	tests := []struct {
-		name    string
-		setup   func() (Provider, error)
-		test    func(t *testing.T, p Provider)
-		wantErr bool
+		name          string
+		usePrivateKey bool
+		clientID      string
+		clientSecret  string
+		wantErr       bool
+		checkRequest  func(t *testing.T, r *http.Request)
 	}{
 		{
-			name: "create provider with client secret",
-			setup: func() (Provider, error) {
-				return OktaFactory(context.Background(), OktaProviderV1, map[string]string{
-					"domain": domain,
-					"scheme": "http", // Add this for test
-				})
-			},
-			test: func(t *testing.T, p Provider) {
-				ops := p.Private("test-client", "test-secret")
-				require.NotNil(t, ops)
-
-				// Test client credentials
-				tok, err := ops.ClientCredentials(context.Background())
+			name:          "private key success",
+			usePrivateKey: true,
+			clientID:      testClientID,
+			wantErr:       false,
+			checkRequest: func(t *testing.T, r *http.Request) {
+				err := r.ParseForm()
 				require.NoError(t, err)
-				assert.Equal(t, "test-access-token", tok.AccessToken)
-				assert.Equal(t, "Bearer", tok.TokenType)
+
+				// Verify common parameters
+				assert.Equal(t, "POST", r.Method)
+				assert.Equal(t, "/oauth2/v1/token", r.URL.Path)
+				assert.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+				assert.Equal(t, "client_credentials", r.Form.Get("grant_type"))
+
+				// Verify JWT parameters
+				assert.Equal(t, "urn:ietf:params:oauth:client-assertion-type:jwt-bearer", r.Form.Get("client_assertion_type"))
+				assert.NotEmpty(t, r.Form.Get("client_assertion"))
 			},
 		},
 		{
-			name: "create provider with private key",
-			setup: func() (Provider, error) {
-				return OktaFactory(context.Background(), OktaProviderV1, map[string]string{
-					"domain":      domain,
-					"private_key": keyPEM,
-					"scheme":      "http", // Add this for test
-				})
-			},
-			test: func(t *testing.T, p Provider) {
-				ops := p.Private("test-client", "")
-				require.NotNil(t, ops)
-
-				// Test client credentials with JWT
-				tok, err := ops.ClientCredentials(context.Background())
-				require.NoError(t, err)
-				assert.Equal(t, "test-access-token", tok.AccessToken)
-				assert.Equal(t, "Bearer", tok.TokenType)
-			},
-		},
-		{
-			name: "auth code flow",
-			setup: func() (Provider, error) {
-				return OktaFactory(context.Background(), OktaProviderV1, map[string]string{
-					"domain": domain,
-					"scheme": "http", // Add this for test
-				})
-			},
-			test: func(t *testing.T, p Provider) {
-				ops := p.Private("test-client", "test-secret")
-				require.NotNil(t, ops)
-
-				// Test auth code URL generation
-				authURL, ok := ops.AuthCodeURL("test-state", WithRedirectURL("http://localhost/callback"))
-				require.True(t, ok)
-				assert.Contains(t, authURL, "/oauth2/v1/authorize")
-				assert.Contains(t, authURL, "client_id=test-client")
-				assert.Contains(t, authURL, "state=test-state")
-
-				// Test auth code exchange
-				tok, err := ops.AuthCodeExchange(context.Background(), "valid-code",
-					WithRedirectURL("http://localhost/callback"))
-				require.NoError(t, err)
-				assert.Equal(t, "test-access-token", tok.AccessToken)
-				assert.Equal(t, "test-refresh-token", tok.RefreshToken)
-			},
-		},
-		{
-			name: "refresh token",
-			setup: func() (Provider, error) {
-				return OktaFactory(context.Background(), OktaProviderV1, map[string]string{
-					"domain": domain,
-					"scheme": "http", // Add this for test
-				})
-			},
-			test: func(t *testing.T, p Provider) {
-				ops := p.Private("test-client", "test-secret")
-				require.NotNil(t, ops)
-
-				oldToken := &Token{
-					Token: &oauth2.Token{
-						AccessToken:  "test-access-token",
-						RefreshToken: "test-refresh-token",
-						TokenType:    "Bearer",
-					},
-					ProviderVersion: OktaProviderV1,
-				}
-
-				// Test refresh token
-				newToken, err := ops.RefreshToken(context.Background(), oldToken)
-				require.NoError(t, err)
-				assert.Equal(t, "new-access-token", newToken.AccessToken)
-				assert.Equal(t, "new-refresh-token", newToken.RefreshToken)
-			},
-		},
-		{
-			name: "token exchange",
-			setup: func() (Provider, error) {
-				return OktaFactory(context.Background(), OktaProviderV1, map[string]string{
-					"domain": domain,
-					"scheme": "http", // Add this for test
-				})
-			},
-			test: func(t *testing.T, p Provider) {
-				ops := p.Private("test-client", "test-secret")
-				require.NotNil(t, ops)
-
-				oldToken := &Token{
-					Token: &oauth2.Token{
-						AccessToken: "test-access-token",
-						TokenType:   "Bearer",
-					},
-					ProviderVersion: OktaProviderV1,
-				}
-
-				// Test token exchange
-				newToken, err := ops.TokenExchange(context.Background(), oldToken)
-				require.NoError(t, err)
-				assert.Equal(t, "exchanged-access-token", newToken.AccessToken)
-			},
+			name:          "missing client secret",
+			usePrivateKey: false,
+			clientID:      testClientID,
+			wantErr:       true,
+			checkRequest:  nil,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p, err := tt.setup()
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if tt.checkRequest != nil {
+					tt.checkRequest(t, r)
+				}
+
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte(`{
+                    "access_token": "test-token",
+                    "token_type": "Bearer",
+                    "expires_in": 3600
+                }`))
+			}))
+			defer server.Close()
+
+			opts := map[string]string{
+				"domain": strings.TrimPrefix(server.URL, "http://"),
+				"scheme": "http",
+			}
+
+			if tt.usePrivateKey {
+				keyPEM, _ := generateTestPKCS8Key(t)
+				opts["private_key"] = keyPEM
+			}
+
+			provider, err := OktaFactory(context.Background(), OktaProviderV1, opts)
+			require.NoError(t, err)
+
+			ops := provider.Private(tt.clientID, tt.clientSecret)
+			token, err := ops.ClientCredentials(context.Background())
+
 			if tt.wantErr {
 				require.Error(t, err)
 				return
 			}
-			require.NoError(t, err)
-			require.NotNil(t, p)
 
-			tt.test(t, p)
+			require.NoError(t, err)
+			require.NotNil(t, token)
+			assert.Equal(t, "test-token", token.AccessToken)
+			assert.Equal(t, "Bearer", token.TokenType)
 		})
 	}
+}
+
+func TestOktaUnsupportedFlows(t *testing.T) {
+	provider, err := OktaFactory(context.Background(), OktaProviderV1, map[string]string{
+		"domain": "test.okta.com",
+	})
+	require.NoError(t, err)
+
+	ops := provider.Private("test-client", "test-secret")
+
+	t.Run("device code auth", func(t *testing.T) {
+		auth, ok, err := ops.DeviceCodeAuth(context.Background())
+		assert.Nil(t, auth)
+		assert.False(t, ok)
+		assert.Nil(t, err)
+	})
+
+	t.Run("device code exchange", func(t *testing.T) {
+		token, err := ops.DeviceCodeExchange(context.Background(), "test-code")
+		assert.Nil(t, token)
+		assert.EqualError(t, err, "device code flow not supported")
+	})
+
+	t.Run("auth code exchange", func(t *testing.T) {
+		token, err := ops.AuthCodeExchange(context.Background(), "test-code")
+		assert.Nil(t, token)
+		assert.EqualError(t, err, "auth code exchange flow not supported")
+	})
+
+	t.Run("token exchange", func(t *testing.T) {
+		token, err := ops.TokenExchange(context.Background(), nil)
+		assert.Nil(t, token)
+		assert.EqualError(t, err, "token exchange flow not supported")
+	})
 }
